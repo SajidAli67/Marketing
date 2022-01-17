d6581305b1f02ba60e557dab0483140756b7d6e6
sajid Ali <sajid.ali67@outlook.com>
2022-01-17 12:14:23 +0500

    Full compalet

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,18 +4,18 @@ const navLink = document.querySelectorAll(".nav-link");
 var load = document.getElementById("loading");
 
 hamburger.addEventListener("click", mobileMenu);
-navLink.forEach(n => n.addEventListener("click", closeMenu));
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
 function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 }
 
 function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
 }
 
-function loadfun(){
-    load.style.display="none";
+function loadfun() {
+  load.style.display = "none";
 }
